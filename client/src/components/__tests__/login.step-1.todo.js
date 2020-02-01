f2e1c40ca188a212671dc7a09b286a66b00b3917
Flavia Costa <flavia.costa@talkdesk.com>
2020-02-01 19:10:16 +0000

    test a form component solution

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from '../login'
 
-test('t',()=> {
+test('calls onSubmit with the username and password when submitted',()=> {
 const handleSubmit = jest.fn()
 const container = document.createElement('div')
 ReactDOM.render(<Login onSubmit={handleSubmit} />, container)
